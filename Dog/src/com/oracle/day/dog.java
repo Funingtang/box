6f92ca21deeecf31e05510ef0a014b606fa481cd
HANXINSTEVEN <3563649052@qq.com>
2020-09-07 14:48:11 +0800

    第一次提交

---
+++ Dog/src/com/oracle/day/dog.java
@@ -0,0 +1,9 @@
+package com.oracle.day;
+
+public class dog {
+	public static void main(String[] args) {
+		System.out.println("qqqqqqqqqqqqqqqqqqq");
+		System.out.println("ccccccccccccccccccc");
+	}
+
+}
